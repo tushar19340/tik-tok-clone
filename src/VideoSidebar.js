7e83d304df03ab47037fc153653f7ab35dead10a
tushar <tushar19340@iiitd.ac.in>
2020-11-20 00:25:18 -0800

    like button added

--- src/VideoSidebar.js
+++ src/VideoSidebar.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './VideoSidebar.css';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import MessageIcon from '@material-ui/icons/Message';
 
 function VideoSidebar() {
+    const [liked, setLiked] = useState(false);
+
   return (
     <div className="videoSidebar">
       <div className="videoSidebar__button">
-        <FavoriteBorderIcon />
+          {liked ? (
+               <FavoriteBorderIcon 
+               onClick={e => setLiked(false)} />
+          ): (
+            <  FavoriteIcon 
+            onClick={e => setLiked(true)} />
+          )}
         <p>300</p>
       </div>
       <div className="videoSidebar__button">
